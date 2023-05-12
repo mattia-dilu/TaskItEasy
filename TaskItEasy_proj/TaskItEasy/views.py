5c156fc882ffe15786388c40454a2f394b1d8a5a
mattia_diluca <diluca.mattia02@gmail.com>
2023-05-12 11:24:05 +0200

    setup registration, login, home and set index page (login page). To be done: 1) check login operation 2) modify registration form 3) create logout and deletetion user 5) change home

--- TaskItEasy_proj/TaskItEasy/views.py
+++ TaskItEasy_proj/TaskItEasy/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django.urls import  reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, username=name, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("home"))
+        else:
+            return render(request, "TaskItEasy/index.html", {
+            "message": "Invalid credentials."
+            })
     return render(request, "TaskItEasy/index.html")
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home') 
+    else:
+        form = UserCreationForm()
+    return render(request, 'TaskItEasy/register.html', {'form': form})
+
+@login_required
+def home(request):
+    return render(request, 'TaskItEasy/home.html')
