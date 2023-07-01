ad6214fbab548b82728508bfb4ab701fcf1f0942
mattia_diluca <diluca.mattia02@gmail.com>
2023-07-01 19:29:07 +0200

    update/implementation: 1) login 2) registration 3) deletetion of user

--- TaskItEasy_proj/TaskItEasy/urls.py
+++ TaskItEasy_proj/TaskItEasy/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views 
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path("", views.index, name="index"),
     path('register/', views.register, name='register'),
     path('home/', views.home, name='home'),
+    path('logout/', views.logout_user, name='logout'),
+    path('delete_user/', views.delete_user, name='delete_user'),
 ]       
--- TaskItEasy_proj/TaskItEasy/views.py
+++ TaskItEasy_proj/TaskItEasy/views.py
@@ -4,14 +4,16 @@ from django.http import HttpResponseRedirect
 from django.urls import  reverse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from .forms import RegistrationForm
 
 # Create your views here.
 def index(request):
     if request.method == "POST":
-        name = request.POST["name"]
-        email = request.POST["email"]
+        username = request.POST["username"]
+        
         password = request.POST["password"]
-        user = authenticate(request, username=name, email=email, password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return HttpResponseRedirect(reverse("home"))
@@ -23,14 +25,27 @@ def index(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('home') 
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
     return render(request, 'TaskItEasy/register.html', {'form': form})
+
+def logout_user(request):
+    logout(request)
+    return redirect('index') 
+
+def delete_user(request):
+    if request.method == 'POST':
+        user = request.user
+        user.delete()
+        return redirect('index')
+    else:
+        return render(request, 'TaskItEasy/delete_user.html')
+                    
 
 @login_required
 def home(request):
